fd8685eca87441d51cd45203d38974cef79f674b
Peter Himschoot <peter@u2u.be>
2025-12-07 14:54:21 +0100

    shoppingRepo is extension methods style

--- src/ModularMonolith.APIs/BoundedContexts/Shopping/Repositories/IShoppingRepository.cs
+++ src/ModularMonolith.APIs/BoundedContexts/Shopping/Repositories/IShoppingRepository.cs
@@ -1,14 +1,14 @@
-﻿using ModularMonolith.APIs.BoundedContexts.Common.Repositories;
-using ModularMonolith.APIs.BoundedContexts.Common.ValueObjects;
+﻿//using ModularMonolith.APIs.BoundedContexts.Common.Repositories;
+//using ModularMonolith.APIs.BoundedContexts.Common.ValueObjects;
 
-namespace ModularMonolith.APIs.BoundedContexts.Shopping.Repositories;
+//namespace ModularMonolith.APIs.BoundedContexts.Shopping.Repositories;
 
-public interface IShoppingRepository
-: IRepository<ShoppingBasket>
-{
-  ValueTask<ShoppingBasket?> GetShoppingBasketAsync(
-    PK<int> i
-  , CancellationToken cancellationToken);
+//public interface IShoppingRepository
+//: IRepository<ShoppingBasket>
+//{
+//  ValueTask<ShoppingBasket?> GetShoppingBasketAsync(
+//    PK<int> i
+//  , CancellationToken cancellationToken);
 
-  ValueTask<int> DeleteOldBasketsAsync(CancellationToken cancellationToken);
-}
+//  ValueTask<int> DeleteOldBasketsAsync(CancellationToken cancellationToken);
+//}
--- src/ModularMonolith.APIs/BoundedContexts/Shopping/Repositories/ShoppingRepository.cs
+++ src/ModularMonolith.APIs/BoundedContexts/Shopping/Repositories/ShoppingRepository.cs
@@ -1,42 +1,12 @@
-﻿
-using System.Xml.Linq;
+﻿namespace ModularMonolith.APIs.BoundedContexts.Shopping.Repositories;
 
-namespace ModularMonolith.APIs.BoundedContexts.Shopping.Repositories;
-
-//internal sealed class ShoppingRepository
-//: Repository<ShoppingBasket, ShoppingDb>
-//, IShoppingRepository
-//{
-//  public ShoppingRepository(
-//    ShoppingDb db
-//    , IDomainEventPublisher domainEventPublisher
-//  ) : base(db, domainEventPublisher) { }
-
-
-//  public async ValueTask<ShoppingBasket?> GetShoppingBasketAsync(
-//    PK<int> shoppingBasketId
-//  , CancellationToken cancellationToken) 
-//  => await SingleAsync(
-//      ShoppingBasketSpecification.WithId(shoppingBasketId)
-//    , cancellationToken);
-
-//  public async ValueTask<int> DeleteOldBasketsAsync(
-//    CancellationToken cancellationToken)
-//  {
-//    return await DbContext
-//      .Baskets
-//      .Where(sb => EF.Property<bool>(sb, SoftDeleteable.IsDeleted) == true)
-//      .ExecuteDeleteAsync(cancellationToken);
-//  }
-
-//  protected override IQueryable<ShoppingBasket> Includes(IQueryable<ShoppingBasket> q)
-//  {
-//    q = q.Include(sb => sb.Items).ThenInclude(it => it.G);
-//    q = q.Include(sb => sb.Customer);
-//    return q;
-//  }
-//}
-
+/// <summary>
+/// Shopping repository using extension method style.
+/// </summary>
+/// <remarks>
+/// By using extension methods we can avoid repeating ourselves while having the power of a repository
+/// with methods to retrieve items from the database.
+/// </remarks>
 internal static class ShoppingRepostory
 {
   extension(ShoppingDb db)
@@ -58,6 +28,14 @@ internal static class ShoppingRepostory
       return query;
     }
 
+    /// <summary>
+    /// Retrieves a shopping basket by its identifier asynchronously.
+    /// </summary>
+    /// <param name="shoppingBasketId"></param>
+    /// <param name="includeGames"></param>
+    /// <param name="includeCustomer"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
     public async ValueTask<ShoppingBasket?> GetShoppingBasketAsync(
       PK<int> shoppingBasketId
     , bool includeGames = false
@@ -69,6 +47,17 @@ internal static class ShoppingRepostory
         .Where(sb => sb.Id == shoppingBasketId)
         .SingleOrDefaultAsync(cancellationToken);
     }
+
+    /// <summary>
+    /// Deletes all baskets that are marked as deleted from the database asynchronously.
+    /// </summary>
+    /// <remarks>
+    /// This method permanently removes baskets that have been soft-deleted. The operation is
+    /// performed in the database and may affect multiple records. If the cancellation token is triggered before
+    /// completion, the operation will be canceled.
+    /// </remarks>
+    /// <param name="cancellationToken">A cancellation token that can be used to cancel the delete operation.</param>
+    /// <returns>A task that represents the asynchronous operation. The task result contains the number of baskets deleted.</returns>
 
     public async ValueTask<int> DeleteOldBasketsAsync(
       CancellationToken cancellationToken)
