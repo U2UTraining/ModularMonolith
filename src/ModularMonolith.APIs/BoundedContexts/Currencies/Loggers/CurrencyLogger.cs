7640587f851046c6e8904ccf2fb4d360129aa387
Peter Himschoot <peter@u2u.be>
2026-05-06 10:40:45 +0200

    Logging

--- src/ModularMonolith.APIs/BoundedContexts/Currencies/Loggers/CurrencyLogger.cs
+++ src/ModularMonolith.APIs/BoundedContexts/Currencies/Loggers/CurrencyLogger.cs
@@ -5,7 +5,7 @@ public static partial class CurrencyLogger
   [LoggerMessage(
         EventId = 123
       , Level = LogLevel.Information
-      , Message = "Updated currency at {timestamp}")]
+      , Message = "[Currencies] Updated currency at {timestamp}")]
   public static partial void UpdateCurrencyValueInEuroCommandInvoked(
         ILogger logger
       , DateTime timestamp
