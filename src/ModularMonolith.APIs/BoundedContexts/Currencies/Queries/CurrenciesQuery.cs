c68c5b52eb5b1e52810e9ca5501f5d2258095045
Peter Himschoot <peter@u2u.be>
2026-05-07 10:42:07 +0200

    Add support to invoke a query without querySender
    This to illustrate that some people want to see
    which module/bounded context they are invoking

---
+++ src/ModularMonolith.APIs/BoundedContexts/Currencies/Queries/CurrenciesQuery.cs
@@ -0,0 +1,13 @@
+﻿namespace ModularMonolith.APIs.BoundedContexts.Currencies.Queries;
+
+[Register(
+  lifetime: ServiceLifetime.Singleton
+, methodNameHint: "AddCurrencyServices")]
+public sealed class CurrenciesQuery(IQuerySender _querySender)
+{
+  public async Task<List<Currency>> GetAllCurrenciesAsync(GetAllCurrenciesQuery query)
+    => await _querySender.AskAsync(query);
+
+  public async Task<PositiveDecimal[]> GetValueForCurrencyAsync(GetValueForCurrencyQuery query)
+    => await _querySender.AskAsync(query);
+}
